3424c684a433aaaff1f21d0657ce8791d29fd7fa
matymcleod <matymcleod@gmail.com>
2021-06-30 00:58:19 +0000

    refactoring of spinner1 into spinner2

--- spinner2.js
+++ spinner2.js
@@ -1,33 +1,8 @@
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
-}, 700);
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 900);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 1100);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-},1300);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   \n'); 
-},1500);
+const sentence = '\r| \r/ \r- \r\\ \r| \r/ \r- \r\\\n';
+let delay = 0;
+for(const char of sentence) {
+  delay += 50;
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay)
+};
